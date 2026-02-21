6edc97ffde5ff9da3a394a5057d23b283fc37b1b
renType <ren80438@gmail.com>
2026-02-21 23:52:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
 
 let messages = [];
 let users = {};
